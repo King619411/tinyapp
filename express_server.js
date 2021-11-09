f710c044302acbc7ad0d0d90a3edc1a7d9b37c29
Walid <walid_douri@hotmail.com>
2021-11-09 16:43:18 -0500

    Deleting URls - Commit

--- express_server.js
+++ express_server.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
 
-app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+function generateRandomString() {
+  return Math.random().toString(36).replace(/[^a-z]+/g, '').substr(0,5);
+  // Google <3
+}
+
+////////////////////////////////////////////////////
+/////////////GET///////////////////////////////////
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -30,8 +41,50 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
+});
+
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL:req.params.longURL};
+  const shortURL = req.params.shortURL;
+  // const shortUrl = req.params.shortURL;
+  // const longUrl = req.query.longurl;
+  // const templateVars = {
+  //   shortURL: shortUrl,
+  //   longURL: urlDatabase[req.params.shortURL] || longUrl
+  // };
+  const templateVars = {
+    shortURL: shortURL, 
+    longURL:urlDatabase[shortURL]
+  };
   res.render("urls_show", templateVars);
 });
 
+////////////////////////////////////////////
+////////////////POST///////////////////////
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  
+  // Log the POST request body to the console
+  const shortURL = generateRandomString(); 
+  const longURL = req.body.longURL
+  const newDataBaseObjects = {...urlDatabase, [shortURL]: longURL};
+  //console.log(newDataBaseObjects);
+  const templateVars = {urls: newDataBaseObjects};
+  res.render("urls_index", templateVars);
+  
+});
+
+//Delete a urlDatabase
+app.post("/urls/:shortURL/delete", (req, res) => {
+  // or create a var 
+  // let newShortURL = req.param.shortURL;
+  delete urlDatabase[req.params.shortURL];
+  //console.log(urlDatabase)
+  res.redirect("/urls") 
+});
